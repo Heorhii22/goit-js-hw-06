58c8f196796da2111a85a10cff3ee0c6860b677a
George Artemov <113346839+Heorhii22@users.noreply.github.com>
2022-12-26 19:34:45 +0200

    тест задачи js

---
+++ index.js
@@ -0,0 +1,40 @@
+// // Slug for url adress
+// // const test = '10 Easy Ways to learn Js';
+// // const message = test.split(" ").join('-').toLowerCase();
+// // console.log(message);
+
+// // 
+// let longest = ''.length;
+// console.log(longest);
+
+// let value = 4;
+// let array = [];
+// let numbers = [1, 2, 3, 4, 5];
+// console.log('Array length is:', numbers.length);
+// for (let i = 0; i < numbers.length; i++) {
+// console.log(numbers[i]);
+// if (numbers[i] > value) {
+//     array.push(numbers[i])
+//     console.log(array);
+// }
+// }
+
+// function filterArray(numbers, value) {
+//     const newArray = [];
+//     for(let i = 0; numbers.length > i; i++){
+       
+//         if (numbers[i]>value){
+//             newArray.push(numbers[i])
+//         }
+      
+//     }
+//   return newArray
+// }
+// const fn = function TotalPrice(items) {
+//     let total = 0;
+//     for (const price of items) {
+//         total += price
+//     }
+//     return `Сумма чисел: ${total}`
+// }
+// console.log(fn([1, 2, 3, 4, 5]))
